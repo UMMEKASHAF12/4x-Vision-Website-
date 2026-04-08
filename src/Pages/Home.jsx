2e32117a12c5991b55bab7c41c6c1fa9b4de16e7
Imama Ansari <ansariimama@yahoo.com>
2026-04-08 21:48:23 +0500

    Footer added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Stats from "../components/Stats";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Navbar />
       <Hero />
       <Stats />
+      <Footer />
     </>
   );
 }
