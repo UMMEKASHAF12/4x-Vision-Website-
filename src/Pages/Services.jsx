3cdade09b1ec38b07ae34e602ad91fbf44ccf5e1
Imama Ansari <ansariimama@yahoo.com>
2026-04-15 08:03:58 +0500

    add footer in services

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,9 +1,12 @@
 import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
 
 const Services = () => {
   return (
     <>
       <Navbar />
+      {/* Add conetnt here */}
+      <Footer />
     </>
   );
 };
