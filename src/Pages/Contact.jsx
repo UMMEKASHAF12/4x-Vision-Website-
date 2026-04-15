9afd53f1f76c93be03dac2cad5ca67eafbae847b
Imama Ansari <ansariimama@yahoo.com>
2026-04-15 08:16:31 +0500

    fix errors

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+const Contact = () => {
+  return (
+    <>                  
+        <Navbar />  
+
+        {/* Add conetnt here */}
+    </>
+  );
+}
+
+export default Contact;
