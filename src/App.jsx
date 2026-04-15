2f43f4c9df44451cabeb894a7967d620e61e5bf1
Imama Ansari <ansariimama@yahoo.com>
2026-04-15 08:02:16 +0500

    fir error in deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Services from "./Pages/Services";
 import "./index.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </Router>
   );
